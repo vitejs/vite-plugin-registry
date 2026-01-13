0649b342784cbfc361376c0dc0b9ff5c7d172a53
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2026-01-13 20:22:33 +0900

    chore: tweak

--- docs/.vitepress/theme/landing/Layout.vue
+++ docs/.vitepress/theme/landing/Layout.vue
@@ -1,11 +1,23 @@
-<script setup>
+<script setup lang="ts">
 import Hero from './Hero.vue'
 import About from './About.vue'
-import Footer from '@components/oss/Footer.vue'
+import { useData } from 'vitepress'
+import type { FooterConfig } from '@voidzero-dev/vitepress-theme'
+import { computed } from 'vue'
+
+const { theme } = useData()
+const footerConfig = computed<FooterConfig>(() => {
+  const footer = theme.value.footer || {}
+  return footer as FooterConfig
+})
 </script>
 
 <template>
   <Hero />
   <About />
-  <Footer heading="Start building" button-text="Browse Plugins" button-link="/plugins/" />
+  <section
+    class="wrapper wrapper--ticks border-t py-5 px-5 md:px-24 flex flex-col md:flex-row gap-3 md:gap-0 justify-between items-start md:items-center"
+  >
+    <p class="text-sm">{{ footerConfig.copyright }}</p>
+  </section>
 </template>
